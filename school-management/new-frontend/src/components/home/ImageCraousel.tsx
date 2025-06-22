a59f763e1adeaebb7fd77a1f994c0fa9f9ed3fd3
pc <meekailkhan123aar@gmail.com>
2025-06-22 23:45:06 +0530

    one more project

--- school-management/new-frontend/src/components/home/ImageCraousel.tsx
+++ school-management/new-frontend/src/components/home/ImageCraousel.tsx
@@ -8,7 +8,6 @@ function ImageCraousel() {
   useEffect(()=>{
     const interval = setInterval(()=>{
       setActiveImg((prev) => (prev + 1) % images.length);
-      console.log("this is interval")
     },5000) 
 
     return () =>  clearInterval(interval)
@@ -18,13 +17,16 @@ function ImageCraousel() {
     <div className='w-[100%] h-[450px] '>
       <div className="absolute w-[100%] h-[450px] z-0   ">
         {images.map((item,index)=>(
-          <div key={index} className={`w-[100%] h-[100%] duration-700 transition-all ease-in-out absolute img-div ${activeImg === index ? "opacity-100 pointer-events-auto scale-100 ": "opacity-0 pointer-events-none scale-90" }`} style={{
+          <div key={index} className={`w-[100%] h-[100%] duration-1000 transition-all ease-in-out absolute img-div ${activeImg === index ? "opacity-100 pointer-events-auto scale-100 ": "opacity-0 pointer-events-none scale-80" }`} style={{
             backgroundImage : `url(${item})`,
             backgroundSize : "cover",
             backgroundPosition :"center center"
           }}>
           </div>
         ))}
+      </div>
+      <div className="absolute">
+        
       </div>
     </div>
   )
